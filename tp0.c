faa15d316378fecffc6cd61ab4dd816c1bf1ef86
Tomas Ferraro <tetferraro@gmail.com>
2019-03-31 16:29:08 -0300

    Resolucion 1: Propuesta basica hecha en la charla

--- tp0.c
+++ tp0.c
@@ -1,10 +1,24 @@
 /*
- * main.c
+ * Resolucion 1 del TP0 [Dificultad Baja]
  *
- *  Created on: 28 feb. 2019
- *      Author: utnso
+ * Buenas! Esta es una resolucion similar hicimos en las charlas.
+ * Tiene varios problemas de disenio y buen criterio de codigo, dado que nos tomamos
+ * un par de licencias para que el workshop sea mas ameno y rapido.
+ * 
+ * Les proponemos tomen esta resolucion como "una que funciona" y los invitamos
+ * a que revisen las resoluciones 2 y 3, que van introduciones varios conceptos nuevos,
+ * tanto desde el lado duro y tecnico, como de la mantenibilidad del codigo y buenas
+ * practicas.
+ * 
+ * Las otras dos resoluciones pueden encontrarlas en otras branches de este mismo repositorio.
+ *
+ * Espero que le saquen provecho a esta implementacion, comparandola no solo en funcionalidades
+ * con las suyas, sino que tambien a nivel disenio y saquen sus propias conclusiones :).
+ *
+ * Saludos!
+ *
+ * Nota: Sacamos todos los caracteres especiales para que se vea simpre bien :P
  */
-
 #include "tp0.h"
 
 int main(void)
@@ -27,17 +41,18 @@ int main(void)
 	//Esta parte la vamos a hacer aca en el main por simpleza.
 
 	//Primero vas a tener que levantar los valores IP y PUERTO del archivo de configuracion con esas claves. Adivina a quien se los tenes que asignar :P
-
+	ip = config_get_string_value(g_config, "IP");
+	puerto = config_get_string_value(g_config, "PUERTO");
 	//Hecho esto, vamos a conectarnos! usamos la funcion ya dada para hacerlo, y le asignamos el entero que devuelve a conexion
-	//crear_conexion(/*ip, puerto*/);
+	conexion = crear_conexion(ip, puerto);
 
 	char* valor;
 
 	//ahora que tenemos la conexion, es hora de empezar a enviar cosas. Primero, el mensaje.
 	//Levantá el valor que le corresponde a CLAVE del archivo de configuracion de nuevo.
-
+	valor = config_get_string_value(g_config, "CLAVE");
 	//Ahora le podés mandar el mensaje al servidor!
-	//enviar_mensaje(/*mensaje, entero de conexion*/)
+	enviar_mensaje(valor, conexion);
 
 	paquete(conexion);
 
@@ -47,10 +62,10 @@ int main(void)
 void iniciar_logger(void)
 {
 	//creemos el log! Este se debe crear con el nombre "tp0.log", se debe mostrar por consola y debe estar en el nivel INFO
-	//g_logger = log_create(/*nombre del archivo, nombre del programa, 1 para mostrar por consola 0 caso contrario, nivel de log deseado */);
+	g_logger = log_create("tp0.log", "TP0", 1, LOG_LEVEL_INFO);
 
 	//logeemos el mensaje "Soy un log"
-	//log_info(/*logger, mensaje*/);
+	log_info(g_logger, "Soy un log");
 }
 
 void leer_config(void)
@@ -58,12 +73,13 @@ void leer_config(void)
 	char* valor;
 
 	//ahora, vamos a abrir el archivo de configuracion "tp0.config"
-	//g_config = config_create(/*path*/);
+	g_config = config_create("tp0.config");
 
 	//tenemos que levantar en valor asociado a la clave "CLAVE" del archivo y asignarselo a la variable valor
-	//config_get_string_value(/*config, clave*/);
+	valor = config_get_string_value(g_config, "CLAVE");
 
 	//Por último, logeame ese valor :)
+	log_info(g_logger, valor);
 }
 
 void leer_consola(void)
@@ -73,17 +89,13 @@ void leer_consola(void)
 	//El primero te lo dejo de yapa
 	leido = readline(">");
 
-	//Los siguiente 4 pasos vas a tener que meterlos en algun tipo de bucle. Te tiro una pista, no es con for ;)
+	while(*leido != '\0') {
+		log_info(g_logger, leido);
+		free(leido);
+		leido = readline(">");
+	}
 
-	//Voy a dejar esto por aca porque quizas complemente al enunciado: http://faq.utnso.com/punteros
-
-		//Primero, logeame lo leido
-
-		//ahora que ya usamos el string, liberamelo con free
-
-		//free NO asigna el valor NULL a los punteros que libera, pero es buena practica asignarselo a tenerlo apuntando a basura
-
-		//volvemos a poner el readline para poder seguir loggeando.
+	free(leido);
 }
 
 void paquete(int conexion)
@@ -93,18 +105,26 @@ void paquete(int conexion)
 	char* leido;
 	t_paquete* paquete = crear_paquete();
 
-	//De nuevo vas a tener que usar readline hasta que te llegue un nulo igual que antes, solo que ahora en lugar de logear, vas a agregar esa cadena al paquete!
-	//Recomiendo revisar bien el enunciado del TP ya que ahi está explicado con detalles
+	leido = readline(">");
 
-	//agregar_a_paquete(/*paquete, mensaje, tamanio*/);
+	while(*leido != '\0') {
+		agregar_a_paquete(paquete, leido, strlen(leido) + 1);
+		free(leido);
+		leido = readline(">");
+	}
 
+	free(leido);
 	//Una vez hayamos terminado de ingresar valores, toca enviarlo
-	//enviar_paquete(/*paquete, conexion*/);
+	enviar_paquete(paquete, conexion);
 
 	//Y no nos olvidemos de borrar el paquete con eliminar_paquete
+	eliminar_paquete(paquete);
 }
 
 void terminar_programa(int conexion)
 {
 	//Y por ultimo, para cerrar, hay que liberar lo que utilizamos (conexion, log y config) con las funciones de las commons y del TP mencionadas en el enunciado
+	liberar_conexion(conexion);
+	log_destroy(g_logger);
+	config_destroy(g_config);
 }
