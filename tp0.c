f79a69fbbb795d0a7d25876828024c1608781a43
Tomas Ferraro <tetferraro@gmail.com>
2019-03-31 16:15:31 -0300

    Resolucion 2 del tp0: Nivel Intermedio

--- tp0.c
+++ tp0.c
@@ -1,14 +1,21 @@
 /*
- * Resolucion 3 del TP0
+ * Resolucion 2 del TP0 [Dificultad Media]
  *
- * Buenas! A diferencia de la resolucion que hicimos en las charlas (la version 1)
- * y su contraparte más "pulida" (version 2), esta resolucion les va a presentar,
- * aparte de una mejora en buenas practicas de programacion (como evitar repetir codigo),
- * con el concepto de Funciones Anidadas (Nested Functions) y punteros a funciones.
- *
- * Esta resolucion es análoga a la version 2, cambiando la parte estructura del
- * esqueleto que ustedes tienen.
- *
+ * Buenas! A diferencia de la resolucion que hicimos en las charlas,
+ * esta resolucion les va a presentar una mejora en buenas practicas de programacion.
+ * Dado que no queriamos abrumarlos durante el Workshops, nos tomamos un par de licencias
+ * de hacer codigo "feo" y poco mantenible.
+ * 
+ * Por lo tanto, esta resolucion plantea tambien otra forma de resolverlo, cambiando la
+ * estructura del esqueleto que ustedes tienen.
+ * Cosas que se cambiaron:
+ * - Se evitaron las variables globales que, como mencionamos en la charla, generan problemas
+ * de mantenibilidad y estabilidad del código.
+ * - Hay transparencia referencial.
+ * - Evitamos comparar strings en funcion de sus caracteres: Usamos funciones de la biblioteca
+ * standard para trabajar strings: strncmp.
+ * - Organizacion del codigo y eliminacion de variables innecesarias.
+ * 
  * Espero que le saquen provecho a esta implementacion, comparandola no solo en funcionalidades
  * con las suyas, sino que tambien a nivel disenio y saquen sus propias conclusiones :).
  *
@@ -57,89 +64,30 @@ t_config* leer_config() {
 	return config_create("tp0.config");
 }
 
-/*
- * Hasta esta funcion, es parecida a la resolucion numero 2. Pero esta
- * ultima tenia un problema grave: la logica de leer strings de consola
- * se encontraba duplicada.
- * 
- * Para solucionar eso vamos a introducir los dos conceptos nuevos, que
- * si bien no forman parte del standard de ANSI C, si forman parte de la
- * implementación de GNU C, que es la que se encuentra en el compilador que
- * usamos por la catedra: GCC. Estos son:
- * 
- * Funciones anidadas (o Nested Functions):
- * ---------------------------------------
- * El compilador nos permite crear funciones locales al contexto de otra funcion.
- * De esta manera, pueden evitar crear muchas funciones auxiliares que se van a usar
- * en un unico lugar, y tener que pensar miles de nombres diferentes para cada una.
- * Algo importante a tener en cuenta, es que las funciones anidadas, mantienen el
- * contexto de la funcion exterior, es decir, que las variables definidas en la funcion
- * exterior son accesibles desde le interior (masomenos, como funcionan las variables globales).
- * 
- * 
- * Punteros a Funcion:
- * ------------------
- * Las funciones no escapan  del concepto de puntero, por como funciona el compilador.
- * Esto implica que llamar a una funcion por su nombre, ejemplo:
- * ```
- * void _soy_funcion() { };
- * 
- * variable = _soy_funcion;
- * ```
- * Es como tener un puntero a "alguna cosa", y podemos definir la variable como:
- * ```
- * void* variable = (void*) _soy_funcion;
- * ```
- * El casteo a "void*" es necesario para que el compilador entienda que es un puntero
- * a "alguna cosa".
- * 
- * Ahora, si podemos asignar a una variable, porque no: pasar una funcion por parametro.
- * Esto nos habilita el orden superior en nuestras funciones.
- */
 void leer_consola(t_log* logger) {
-	void loggear(char* leido) {
-		log_info(logger, leido);
-	}
-
-	_leer_consola_haciendo((void*) loggear);
-}
-
-t_paquete* armar_paquete() {
-	t_paquete* paquete = crear_paquete();
-
-	void _agregar(char* leido) {
-		// Estamos aprovechando que podemos acceder al paquete
-		// de la funcion exterior y podemos agregarle lineas!
-		agregar_a_paquete(paquete, leido, strlen(leido) + 1);
-	}
-
-	_leer_consola_haciendo((void*) _agregar);
-
-	return paquete;
-}
-
-/*
- * Este comportamiento es el comun a ambas funciones,
- * cambiando solamente que por un lado vamos a loggear y
- * por el otro, queremos agregar las lineas a un paquete.
- * 
- * Por lo tanto, necesita que se le pase una funcion que reciba
- * un string por parametro y no haga nada.
- * 
- * Noten que la firma de la funcion pide void(*accion)(char*)
- * que implica:
- * - Un puntero a la funcion accion: (*accion)
- * - Devuelve un void
- * - Recibe un solo argumento (char*)
- */
-void _leer_consola_haciendo(void(*accion)(char*)) {
 	char* leido = readline(">");
 
 	while(strncmp(leido, "", 1) != 0) {
-		accion(leido);
+		log_info(logger, leido);
 		free(leido);
 		leido = readline(">");
 	}
 
 	free(leido);
+}
+
+t_paquete* armar_paquete() {
+	t_paquete* paquete = crear_paquete();
+
+	char* leido = readline(">");
+
+	while(strncmp(leido, "", 1) != 0) {
+		agregar_a_paquete(paquete, leido, strlen(leido) + 1);
+		free(leido);
+		leido = readline(">");
+	}
+
+	free(leido);
+
+	return paquete;
 }
--- tp0.h
+++ tp0.h
@@ -21,6 +21,4 @@ t_log* iniciar_logger(void);
 t_config* leer_config(void);
 void leer_consola(t_log* logger);
 t_paquete* armar_paquete();
-
-void _leer_consola_haciendo(void(*accion)(char*));
 #endif /* TP0_H_ */
