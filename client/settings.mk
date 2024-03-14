7c4d7125843869dc8f90aabc0a0b2cb7ab9e00ce
Agustin Ranieri <aguseranieri@gmail.com>
2024-03-14 20:58:51 -0300

    Migro estructura base a so-project-template

---
+++ client/settings.mk
@@ -0,0 +1,16 @@
+# Libraries
+LIBS=commons pthread readline m
+
+# Custom libraries' paths
+STATIC_LIBPATHS=
+
+# Compiler flags
+CDEBUG=-g -Wall -DDEBUG -fdiagnostics-color=always
+CRELEASE=-O3 -Wall -DNDEBUG -fcommon
+
+# Arguments when executing with start, memcheck or helgrind
+ARGS=
+
+# Valgrind flags
+MEMCHECK_FLAGS=--track-origins=yes
+HELGRIND_FLAGS=
