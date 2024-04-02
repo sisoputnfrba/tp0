05239116930bb7e067fcde1c21e9f077de5b44cd
Ignacio Pringles <ipringles@frba.utn.edu.ar>
2024-04-01 21:00:07 -0300

    Implementación esperar_cliente

--- server/src/utils.c
+++ server/src/utils.c
@@ -34,11 +34,8 @@ int iniciar_servidor(void)
 
 int esperar_cliente(int socket_servidor)
 {
-	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
-
 	// Aceptamos un nuevo cliente
-	int socket_cliente;
+	int socket_cliente = accept(socket_servidor, NULL, NULL);
 	log_info(logger, "Se conecto un cliente!");
 
 	return socket_cliente;
