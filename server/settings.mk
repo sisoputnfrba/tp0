fde68e91934dd09e5636e323a92b4cd3dd46553e
Agustin Ranieri <aguseranieri@gmail.com>
2024-08-11 17:52:22 -0300

    Update for 2c-2024

--- server/settings.mk
+++ server/settings.mk
@@ -2,15 +2,9 @@
 LIBS=commons pthread readline m
 
 # Custom libraries' paths
+SHARED_LIBPATHS=
 STATIC_LIBPATHS=
 
 # Compiler flags
 CDEBUG=-g -Wall -DDEBUG -fdiagnostics-color=always
-CRELEASE=-O3 -Wall -DNDEBUG -fcommon
-
-# Arguments when executing with start, memcheck or helgrind
-ARGS=
-
-# Valgrind flags
-MEMCHECK_FLAGS=--track-origins=yes
-HELGRIND_FLAGS=
+CRELEASE=-O3 -Wall -DNDEBUG
